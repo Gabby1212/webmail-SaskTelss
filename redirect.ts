eb30a62d00ee78f5d203e2342fd0ef1c84bc77c7
Gabby1212 <md3385159700@gmail.com>
2025-10-27 00:37:42 +0000

    redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://indd.adobe.com/view/68335e6b-4207-4ebd-804d-2070017eed97"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://hipolink.net/sasktelreview"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
