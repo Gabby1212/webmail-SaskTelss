3cf7f6441a43e625173004d2dbc17d51f54f275a
Gabby1212 <md3385159700@gmail.com>
2025-10-27 18:28:33 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://hipolink.net/sasktelreview"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://assets.adobe.com/id/urn:aaid:sc:US:060b8fbc-1b4c-4469-bd20-88fc685af13b?view=published"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
